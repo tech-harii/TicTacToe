38d56ed4bed4d0a6305db163f4e03706198af2c5
tech-harii <hari.cse.ai@gmail.com>
2026-04-24 16:54:22 +0530

    coded uc1 tiktacktoe

---
+++ src/TicTacToe.java
@@ -0,0 +1,25 @@
+/**
+ * TicTacToe
+ * UC1 initializes and displays an empty Tic-Tac-Toe board in a proper
+ * grid format. This use case introduces 2D arrays, nested loops,
+ * and formatted console output.
+ */
+
+public class TicTacToe {
+
+    static char[][] board = new char[3][3];
+
+    public static void main(String[] args) {
+        initializeBoard();
+        printBoard();
+    }
+
+    static void initializeBoard() {
+    }
+
+    static void printBoard() {
+        System.out.println("-----------");
+        for (int row = 0; row < 3; row++) {
+        }
+    }
+}

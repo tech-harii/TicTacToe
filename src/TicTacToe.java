c9cf0ed6f89ca5f1bd6e3337de0c5b8c67c68368
tech-harii <hari.cse.ai@gmail.com>
2026-04-24 17:40:58 +0530

    coded UC3

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,14 +1,49 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class TicTacToe {
+
+    static char[][] board = new char[3][3];
 
     static boolean isHumanTurn;
     static char humanSymbol;
     static char computerSymbol;
 
     public static void main(String[] args) {
+
+        initializeBoard();
         tossAndAssignSymbols();
-        displayTossResult();
+        printBoard();
+
+        int slot = getUserSlot();
+
+        if (isHumanTurn) {
+            placeMove(slot, humanSymbol);
+        } else {
+            placeMove(slot, computerSymbol);
+        }
+
+        printBoard();
+    }
+
+    static void initializeBoard() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                board[row][col] = '_';
+            }
+        }
+    }
+
+    static void printBoard() {
+        System.out.println("-----------");
+        for (int row = 0; row < 3; row++) {
+            System.out.print("| ");
+            for (int col = 0; col < 3; col++) {
+                System.out.print(board[row][col] + " | ");
+            }
+            System.out.println();
+            System.out.println("-----------");
+        }
     }
 
     static void tossAndAssignSymbols() {
@@ -36,5 +71,19 @@ public class TicTacToe {
 
         System.out.println("Your symbol: " + humanSymbol);
         System.out.println("Computer symbol: " + computerSymbol);
+    }
+
+    static int getUserSlot() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a slot number (1-9): ");
+        int slot = scanner.nextInt();
+        return slot;
+    }
+
+    static void placeMove(int slot, char symbol) {
+        int row = (slot - 1) / 3;
+        int col = (slot - 1) % 3;
+
+        board[row][col] = symbol;
     }
 }

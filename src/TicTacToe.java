920a687e4776852da09bfcafa2e89073161a264c
tech-harii <hari.cse.ai@gmail.com>
2026-04-27 19:53:22 +0530

    coded UC4

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,89 +1,17 @@
-import java.util.Random;
-import java.util.Scanner;
+class TicTacToe {
 
-public class TicTacToe {
+    static int getRowFromSlot(int slot) {
+        return (slot - 1) / 3;
+    }
 
-    static char[][] board = new char[3][3];
-
-    static boolean isHumanTurn;
-    static char humanSymbol;
-    static char computerSymbol;
+    static int getColFromSlot(int slot) {
+        return (slot - 1) % 3;
+    }
 
     public static void main(String[] args) {
+        int slot = 7;
 
-        initializeBoard();
-        tossAndAssignSymbols();
-        printBoard();
-
-        int slot = getUserSlot();
-
-        if (isHumanTurn) {
-            placeMove(slot, humanSymbol);
-        } else {
-            placeMove(slot, computerSymbol);
-        }
-
-        printBoard();
-    }
-
-    static void initializeBoard() {
-        for (int row = 0; row < 3; row++) {
-            for (int col = 0; col < 3; col++) {
-                board[row][col] = '_';
-            }
-        }
-    }
-
-    static void printBoard() {
-        System.out.println("-----------");
-        for (int row = 0; row < 3; row++) {
-            System.out.print("| ");
-            for (int col = 0; col < 3; col++) {
-                System.out.print(board[row][col] + " | ");
-            }
-            System.out.println();
-            System.out.println("-----------");
-        }
-    }
-
-    static void tossAndAssignSymbols() {
-        Random random = new Random();
-
-        isHumanTurn = random.nextBoolean();
-
-        if (isHumanTurn) {
-            humanSymbol = 'X';
-            computerSymbol = 'O';
-        } else {
-            humanSymbol = 'O';
-            computerSymbol = 'X';
-        }
-    }
-
-    static void displayTossResult() {
-        if (isHumanTurn) {
-            System.out.println("You won the toss!");
-            System.out.println("You play first.");
-        } else {
-            System.out.println("Computer won the toss!");
-            System.out.println("Computer plays first.");
-        }
-
-        System.out.println("Your symbol: " + humanSymbol);
-        System.out.println("Computer symbol: " + computerSymbol);
-    }
-
-    static int getUserSlot() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a slot number (1-9): ");
-        int slot = scanner.nextInt();
-        return slot;
-    }
-
-    static void placeMove(int slot, char symbol) {
-        int row = (slot - 1) / 3;
-        int col = (slot - 1) % 3;
-
-        board[row][col] = symbol;
+        System.out.println("Row: " + getRowFromSlot(slot));
+        System.out.println("Column: " + getColFromSlot(slot));
     }
 }

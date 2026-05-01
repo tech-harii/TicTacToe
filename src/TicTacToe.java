25e0d99b3ef6261f7c35cdf6bd3a5aefdafcaf7f
tech-harii <hari.cse.ai@gmail.com>
2026-05-01 14:32:59 +0530

    coded UC8

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,72 +1,38 @@
 /**
-@version 7.0
+@version 8.0
 @author Harii
  */
-import java.util.Random;
-
 public class TicTacToe {
 
-    static char[][] board = {
-            {'-', '-', '-'},
-            {'-', '-', '-'},
-            {'-', '-', '-'}
-    };
-
-    static char computerSymbol = 'O';
+    static boolean isHumanTurn = true;
+    static boolean gameOver = false;
 
     public static void main(String[] args) {
-        computerMove();
-        printBoard();
-    }
 
-    static void computerMove() {
-        Random rand = new Random();
-        int slot, row, col;
+        while (!gameOver) {
 
-        while (true) {
-            slot = rand.nextInt(9) + 1;  // 1–9
+            if (isHumanTurn) {
+                System.out.println("Human Turn");
+            } else {
+                System.out.println("Computer Turn");
+            }
 
-            row = getRowFromSlot(slot);
-            col = getColFromSlot(slot);
-
-            if (isValidMove(row, col)) {
-                placeMove(row, col, computerSymbol);
-                break;
+            if (checkWin() || checkDraw()) {
+                gameOver = true;
+            } else {
+                // switch turn
+                isHumanTurn = !isHumanTurn;
             }
         }
+
+        System.out.println("Game Over");
     }
 
-    // UC4: slot → row
-    static int getRowFromSlot(int slot) {
-        return (slot - 1) / 3;
+    static boolean checkWin() {
+        return false;
     }
 
-    // UC4: slot → col
-    static int getColFromSlot(int slot) {
-        return (slot - 1) % 3;
-    }
-
-    // UC5: validation
-    static boolean isValidMove(int row, int col) {
-        if (row < 0 || row > 2 || col < 0 || col > 2) return false;
-        return board[row][col] == '-';
-    }
-
-    // UC6: place move
-    static void placeMove(int row, int col, char symbol) {
-        board[row][col] = symbol;
-    }
-
-    // Utility: print board
-    static void printBoard() {
-        System.out.println("-------------");
-        for (int i = 0; i < 3; i++) {
-            System.out.print("| ");
-            for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " | ");
-            }
-            System.out.println();
-            System.out.println("-------------");
-        }
+    static boolean checkDraw() {
+        return false;
     }
 }
